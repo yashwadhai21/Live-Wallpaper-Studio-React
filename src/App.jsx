dce53e771e75d0b700449e4afe5037b1270e0470
Yashwadhai <yashwadhai21@gmail.com>
2026-04-07 21:51:52 +0530

    api udpations

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { fetchPhotos, fetchVideos } from "./api/mediaApi"
+
 
 const App = () => {
-  return (
-    <div className='h-screen w-full bg-gray-950'>
 
+  return (
+    <div className='h-screen text-white w-full bg-gray-950'>
+      <button className="bg-green-400 px-4 py-2 m-5"
+       onClick={async()=>{
+        const data =await fetchPhotos('cat')
+        console.log(data.results)
+      }}>Get Photos</button>
+
+      <button className="bg-green-400 px-4 py-2 m-5"
+      onClick={async()=>{
+        const data =await fetchVideos('cat')
+        console.log(data.videos)
+      }}>Get Videos</button>
     </div>
   )
 }
