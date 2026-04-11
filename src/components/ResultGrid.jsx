225f1e42fc0a2684eb553a64670bb1a0170df266
Yashwadhai <yashwadhai21@gmail.com>
2026-04-11 10:21:22 +0530

    getting data and updated useEffect

--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -1,29 +1,69 @@
 import { useDispatch, useSelector } from 'react-redux'
 import {fetchPhotos,fetchVideos,fetchGif} from '../api/mediaApi'
-import {setQuery,setLoading,setError,setResults} from '../redux/features/searchSlice'
+import {setLoading,setError,setResults} from '../redux/features/searchSlice'
+import {useEffect } from 'react'
+import ResultCard from './ResultCard'
 
 const ResultGrid = () => {
 
+    const dispatch = useDispatch()
     const {query , activeTab , results , loading , error} = useSelector ((store) => store.search)
     
-    let data 
-    const getData = async ()=>{
+
+    useEffect(function(){
+        if(!query) return 
+         const getData = async ()=>{
+        try {
+            dispatch(setLoading())
+            let data =[]
         if(activeTab == 'photos'){
-            data = await fetchPhotos(query)
-            console.log(data)
+            let response = await fetchPhotos(query)
+            data = response.results.map((item)=>({
+                id:item.id,
+                type:'photo',
+                title:item.alt_description,
+                thumbnail:item.urls.small,
+                src:item.urls.full
+            }))
         }
         if(activeTab == 'videos'){
-            data = await fetchVideos(query)
-            console.log(data)
+            let response = await fetchVideos(query)
+            data = response.videos.map((item)=>({
+                id:item.id,
+                type:'video',
+                title:item.user.name || 'video',
+                thumbnail:item.image,
+                src:item.video_files[0].link
+            }))
         }
         if(activeTab == 'gif'){
-            data = await fetchGif(query)
-            console.log(data)
+            let response = await fetchGif(query)
+            data = response.videos.map((item)=>({
+                id:item.id,
+                type:'GIF',
+                title:item.user.name || 'Gif',
+                thumbnail:item.image,
+                src:item.video_files[0].link
+            }))
+        }
+        console.log(data)
+        dispatch(setResults(data))
+        } catch (err) {
+            dispatch(setError(err.message))
         }
     }
+        getData()
+    },[query,activeTab])
+
+    if(error) return <h1>Error</h1>
+    if(loading) return <h1>Loading...</h1>
   return (
     <div>
-        <button onClick={getData}>getData</button>
+        {results.map((item,idx)=>{
+            return <div key={idx}>
+                        <ResultCard item={item}/>
+                    </div>
+        })}
     </div>
   )
 }
